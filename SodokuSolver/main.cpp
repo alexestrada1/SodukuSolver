6dc81522fa7c2b9d0bb2a97438763072ea205c36
alexestrada1 <estradaalex46@gmail.com>
2024-05-12 02:56:59 -0500

    Created a very rough image of the board for later. Used a 2 array of int pointers initalized with nullpntr. WIll work on getting input from the player to enter a number in the chosed cell.

--- SodokuSolver/main.cpp
+++ SodokuSolver/main.cpp
@@ -1,14 +1,38 @@
-//
-//  main.cpp
-//  SodokuSolver
-//
-//  Created by Alex Estrada on 5/10/24.
-//
-
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Testing commit!\n";
-    return 0;
+using namespace std;
+
+
+
+int main() {
+    int* board[9][9] = {};
+    
+    for (int i = 0 ; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            board[i][j] = nullptr; // Initialize to some default value
+        }
+    }
+    
+    board[1][2] = new int(1);
+    board[0][0] = new int(1);
+    
+    
+    for (int i = 0 ; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            if(j == 0){
+                cout << "| ";
+            }
+            
+            if(board[i][j] != nullptr){
+                cout << " " << *board[i][j] << "  |  ";
+            }
+            else{
+                cout << "    |  ";
+            }
+            
+        }
+        cout << " " << endl << "-----------------------------------------------------------------" << endl;
+        
+    }
 }
