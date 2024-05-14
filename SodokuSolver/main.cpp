f371fdda4961b31dc464f76ce99ad6407315657d
alexestrada1 <estradaalex46@gmail.com>
2024-05-13 23:29:37 -0500

    Made a print board function and added the ability for the user to enter thier choice of number and where on the board. Next will add a delete function and code that will stop them from making incorrect choices. There will also be a restart button added. Then creating the game will start. Afterwards, the algorithm to solve it will come.

--- SodokuSolver/main.cpp
+++ SodokuSolver/main.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-
-
-int main() {
-    int* board[9][9] = {};
-    
-    for (int i = 0 ; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
-            board[i][j] = nullptr; // Initialize to some default value
-        }
-    }
-    
-    board[1][2] = new int(1);
-    board[0][0] = new int(1);
-    
+void printBoard(int* board[][9]){
     
     for (int i = 0 ; i < 9; i++){
         for(int j = 0; j < 9; j++){
@@ -35,4 +24,50 @@ int main() {
         cout << " " << endl << "-----------------------------------------------------------------" << endl;
         
     }
+}
+
+int main() {
+    int* board[9][9] = {};
+    
+    for (int i = 0 ; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            board[i][j] = nullptr; // Initialize to some default value
+        }
+    }
+    board[1][2] = new int(1);
+    board[0][0] = new int(1);
+    
+    printBoard(board);
+    
+    string choice = "";
+    cout << "Enter what ineger you would like to enter in the board. Do this by"
+    << "entering it in interger + Row + colum " << endl;
+    cout << "Ex: 123 would make 1 go into the 2nd row and third column" << endl << "type n to cancel"<< endl;
+    
+    while(choice != "n"){
+        cin >> choice;
+        if (choice == "n") {
+                  std::cout << "Cancelled." << std::endl;
+                  break; // Exit the loop if 'n' is entered
+              }
+        
+        int number, row, column;
+        int choices[3];
+        for (int i= 0; i < choice.length(); i++) {
+            choices[i] = choice.at(i) - '0';
+        }
+        number = choices[0];
+        row = choices[1] -1;
+        column = choices[2] -1; //-1 to let them put the number in the chosen spot. Array starts at 0
+        
+        if(board[row][column] != nullptr){
+            delete board[row][column]; // Delete existing value if any
+
+        }
+        board[row][column] = new int(number);
+
+                printBoard(board);
+              std::cout << "You entered: " << choice << std::endl;
+          }
+    
 }
